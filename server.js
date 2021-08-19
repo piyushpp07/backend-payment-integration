8c1ff3ea43fb185e87cf70c7d356a7c488f2a2df
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2021-08-19 14:17:13 +0530

    backend

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const cors = require("cors");
 const express = require("express");
 const stripe = require("stripe")("sk_test_51JObFKSAm54TGSWjFYtH6uivWmQClLYDdXu51tCxOAn2eSNITrZWre6AjEUbQQ7kYh8dnJiq0mF0dSOOnpgK1WS900vfYdpnqV");
-const uuid = require("uuid/v4");
+const { v4: uuidv4 } = require('uuid');
+// uuidv4();
 
 const app = express();
 
@@ -18,22 +19,34 @@ app.post("/checkout", async (req, res) => {
     let error;
     let status;
     try {
-        const { product, token } = req.body;
+        const { product, token, amount } = req.body;
 
         const customer = await stripe.customers.create({
             email: token.email,
             source: token.id
         });
 
-        const idempotency_key = uuid();
+        const idempotencyKey = uuidv4();
         const charge = await stripe.charges.create(
             {
-                amount: product.price * 100,
-
-
+                amount: amount * 100,
+                currency: "inr",
+                customer: customer.id,
+                receipt_email: token.email,
+                description: `Purchased the ${product.name}`,
+                // shipping: {
+                //   name: token.card.name,
+                //   address: {
+                //     line1: token.card.address_line1,
+                //     line2: token.card.address_line2,
+                //     city: token.card.address_city,
+                //     country: token.card.address_country,
+                //     postal_code: token.card.address_zip
+                //   }
+                // }
             },
             {
-                idempotency_key
+                idempotencyKey
             }
         );
         console.log("Charge:", { charge });
@@ -46,4 +59,6 @@ app.post("/checkout", async (req, res) => {
     res.json({ error, status });
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 5000)
+console.log("Server Running")
+
